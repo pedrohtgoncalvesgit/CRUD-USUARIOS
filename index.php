5673c80fa05175516399d53d0f22c7af3a5ca83d
Pedro <pedrottorres07@gmail.com>
2025-02-03 11:37:25 -0300

    Criando nav e as tabelas de usuarios

---
+++ index.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Crud JS</title>
+  </head>
+  <body>
+        <!-- Incluindo o Nav  -->
+    <?php include ('nav.php'); ?>
+
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                    <h4>Listar Usuarios
+                        <a class="btn btn-primary float-end" href="usuariocreate.php">Adicionar usuario</a> </h4>
+                </div>
+                <div class="card-body">
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>NOME</th> 
+                                <th>EMAIL</th>
+                                <th>DATA NASCIMENTO</th>
+                                <th>AÇÕES</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Pedro</td>
+                                <td>pedro@gmail.com</td>
+                                <td>14/12/2004</td>
+                                <td>
+                                    <a class="btn btn-primary btn-sm" href="">Visualizar</a>
+                                    <a class="btn btn-warning btn-sm text-white" href="">Editar</a>
+                                    <form action="" method="post" class="d-inline">
+                                    <button type="submit" class="btn btn-danger btn-sm" name="delete_usuarios">
+                                        Excluir
+                                    </button></form>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
---
+++ nav.php
@@ -0,0 +1,5 @@
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container-md">
+        <a class="navbar-brand" href="">CRUD - USUARIOS </a>
+    </div>
+</nav>
